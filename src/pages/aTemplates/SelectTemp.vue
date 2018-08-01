7f49401a12d0bc28c7efdf645fcf381709510801
huangjian <982045468@qq.com>
2018-08-01 14:30:46 +0800

    修改部分网站模板

--- src/pages/aTemplates/SelectTemp.vue
+++ src/pages/aTemplates/SelectTemp.vue
@@ -1,11 +1,18 @@
 <template>
     <div>
-      <el-row>
-         <el-col :span="24">选择模板</el-col>
+      <el-row class="mt30 c">
+         <el-col :span="24"><strong>选择模板</strong></el-col>
       </el-row>
       <el-row :gutter="20">
-        <el-col :span="24" class="bg-purple-dark grid-content" >
-
+        <el-col :span="6" class="grid-content" :offset="3">
+          <img src="../../common/img/temp1.png" class="a">
+          <el-button type="primary" class="b">模板一</el-button>
+          <el-button type="success" @click="jump">预览</el-button>
+        </el-col>
+        <el-col :span="6" class="grid-content" :offset="3">
+          <img src="../../common/img/temp1.png" class="a">
+          <el-button type="primary" class="b">模板二</el-button>
+          <el-button type="success" @click="jump">预览</el-button>
         </el-col>
       </el-row>
 
@@ -22,7 +29,9 @@
         }
       },
       methods: {
-
+         jump(){
+           this.$router.push('/templates')
+         }
 
       },
       components:{
@@ -38,6 +47,19 @@
   .grid-content {
     border-radius: 4px;
     min-height: 36px;
+    border: 1px grey solid;
   }
-
+  .mt30{margin-top: 30px;}
+  .a{
+    width: 100%;
+    height: 250px;
+  }
+  .b{
+    margin-top: 15px;
+    margin-left: 15px;
+    margin-bottom: 10px;
+  }
+  img{margin-top: 10px;}
+  strong{font-size: 1.2em;}
+  .c{margin-left: 50px;}
 </style>
