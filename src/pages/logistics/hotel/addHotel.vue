5604262456a4d15767f92d4eb287fc0d96323e7f
sunjia <1170407879@qq.com>
2018-10-19 13:17:45 +0800

    "权限管理前台页面"

--- src/pages/logistics/hotel/addHotel.vue
+++ src/pages/logistics/hotel/addHotel.vue
@@ -1,163 +1,118 @@
 <template>
-  <div class="display1">
-    <button class="btn1 btn2" >
-      添加房间
-      <router-link to="/logistics/hotel" v-bind:style="{ fontSize: '14px' }">
-        (返回列表)
-      </router-link>
-    </button>
-    <div class="display2">
-      <div class="col-md-12">
-        <h3>
-          酒店名称：
-          <input type="text" v-model="roomList.hotelName" />
-        </h3>
-        <div class="roomContainer">
-          <div class="roomType">单人间</div>
-          <div class="roomContent">
-            <input type="text" v-model="roomList.siginalList" />
-          </div>
-        </div>
-        <hr>
-        <div class="roomContainer">
-          <div class="roomType">双人标间</div>
-          <div class="roomContent">
-            <input type="text" v-model="roomList.dobuleList" />
-          </div>
-        </div>
-        <hr>
-        <div class="roomContainer">
-          <div class="roomType">大床房</div>
-          <div class="roomContent">
-            <input type="text" v-model="roomList.bigList" />
-          </div>
-        </div>
-        <hr>
-        <div class="roomContainer">
-          <div class="roomType">三人房</div>
-          <div class="roomContent">
-            <input type="text" v-model="roomList.threeList" />
-          </div>
-        </div>
-      </div>
-      <button class="btn3">保存</button>
+  <div >
+    <div class="top">
+      <span class="">填写酒店信息</span>
+      <hr>
     </div>
+    <div class="form">
+      <span >酒店名称：<input v-model="hotelName" placeholder=""></span> <br>
+      <span >联系电话：<input v-model="hotelTel" placeholder=""></span> <br>
+      <span >负责人：<input v-model="charger" placeholder=""></span> <br>
+      <span >酒店位置：<input v-model="position" placeholder=""></span> <br>
+    </div>
+    <button  class="btn2 icon-huanyihuan" v-on:click="reset()">重置</button>
+    <button  class="btn1 icon-duigou"  v-on:click="submit(hotelName,hotelTel,charger,position)">确认</button>
   </div>
-
 </template>
 
 <script>
-  export default {
-    name: 'bus',
-    data() {
-      return {
-        roomList: {
-          hotelName:"",
-          siginalList:[],
-          dobuleList:[],
-          bigList:[],
-          threeList:[]
-        },
-        isActive:true
-      }
-    },
-    methods: {
-      getHotelInfo:function(){
-        //这里写请求的代码
+    export default {
+      name: "Addhotel",
+      data(){
+        return {
+          hotelName:'',
+          hotelTel:'',
+          charger:'',
+          position:'',
+        }
       },
-    },
-  }
+      methods:{
+        reset:function(){
+          this.hotelName='';
+          this.hotelTel='';
+          this.charger='';
+          this.position='';
+        },
+        submit:function (hotelName,hotelTel,charger,position) {
+          console.log(hotelName,hotelTel,charger,position);
+          if (hotelName!=''&&hotelTel!=''&&charger!=''&&position!=''){
+            this.$http.post('/yii/logistics/hotel/inserthoteldata', {
+                name:hotelName,
+                tel:hotelTel,
+                charger:charger,
+                position:position
+            }).then(function(res){
+              alert("酒店信息添加成功！");
+              window.location.href = '/logistics/hotel'
+            });
+          }else {
+            if (hotelName==''){
+              alert("酒店名称不能为空！");
+            }else if (hotelTel==''){
+              alert("联系号码不能为空！");
+            }else if (charger==''){
+              alert("负责人不能为空！");
+            }else if (position==''){
+              alert("酒店位置不能为空！");
+            }
+          }
+        }
+      }
+    }
 </script>
 
 <style scoped>
-  input{
-    padding: 0 10px;
-    margin: 0;
-    height: 28px;
-    line-height: 28px;
-    width: 50%;
-  }
-  .display1{
-    padding-left:20px;
-    padding-top:10px;
-  }
-  .display2{
-    border:solid 2px  #e0e0e0;
-    height: 500px;
-    /*text-align: center;*/
-    width: 98%;
-    padding-left:10px;
-    padding-right:10px;
-    background-color:  #fff;
-  }
-  table{
-    border-collapse: collapse;
-    width:100%;
-    margin-top: 10px;
-    margin-left: 20px;
-  }
-  th{
-    font-size: 14px;
-    border:solid 1px #ccc;
+  .top{
+    font-size: 20px;
     font-weight: bold;
-    padding:5px;
-    background-color: #F1F1F1;
+    padding-top:20px;
+    padding-left: 20px;
   }
-  table,td{
-    border:solid 1px #ccc;
-    padding:5px;
-    text-align: center;
-    font-size:14px;
+  .form{
+    padding:20px 0px 20px 20px;
+    border:solid 1px #EBEBEC;
+    background-color: #F7F7F7;
+    width:90%;
+    font-size: 16px;
+  }
+  input{
+    width:110px;
+    margin-top: 8px;
+    font-size: 14px;
+  }
+  select{
+    margin-top: 8px;
+    font-size: 14px;
   }
   .btn1{
-    font-size: 18px;
-    padding: 10px 10px;
-    border-top-left-radius: 3px;
-    border-top-right-radius: 3px;
-    border: 1px solid #ccc;
-    cursor: pointer;
-    background: #f0f0f0;
-    margin-bottom: -1px;
-    color:#000;
-    /*margin-right: 0px;*/
-  }
-  .btn1:hover{
-    background: #e0e0e0;
-  }
-  .active{
-    background: #e0e0e0;
-  }
-  .btn2{
-    margin-left: 0px;
-  }
-  .btn3{
     width:80px;
     padding:7px;
-    font-size: 14px;
+    font-size: 16px;
     border-radius: 3px;
     border:none;
     color:white;
     background-color:#338FFC ;
-    margin-left: 15px;
-    margin-right: 15px;
-    margin-top:13px;
+    margin-left: 20px;
+    margin-top: 15px;
+    /*margin-bottom: 5px;*/
   }
-  .btn3:hover{
+  .btn1:hover{
     background-color:#5FA7FE;
   }
-  .roomType{
-    width: 10%;
-    display:inline-block;
-    line-height: 30px;
-    min-height: 30px;
-    background: #eee;
-    margin: 0;
+  .btn2{
+    width:80px;
+    padding:7px;
+    font-size: 16px;
+    border-radius: 3px;
+    border:none;
+    color:white;
+    background-color:#898989;
+    /*float: right;*/
+    margin-left: 20px;
+    margin-top: 5px;
+    /*margin-bottom: 5px;*/
   }
-  .roomContent{
-    width: 70%;
-    display:inline-block;
-    line-height: 30px;
-    min-height: 60px;
-    margin: 0;
+  .btn2:hover{
+    background-color:#A5A5A5;
   }
 </style>
